419648d4d8627e18afccb4faeaae56d9de6e4458
@jmarcos-cano <jmarcos.cano@gmail.com>
2020-10-05 09:05:35 -0600

    Hide env varibales. @jmarcos-cano - wipe

--- comenzar-aqui/myapp.sh
+++ comenzar-aqui/myapp.sh
@@ -8,12 +8,12 @@
 # env_var=${VARIABLE}
 
 #? pasa leer variable de entorno y asignar un valor default si no existe
-# :${VARIABLE:-"default"}
+: ${env_var:="default msg"}
 
 
 count=0
 while true;do
-  echo "[${count}] Hola Container bootcamp experts! 🐳 🤓"
+  echo "[${count}] Hola Container bootcamp experts! 🐳 🤓 -> ${env_var}"
   count=$(( count + 1))
   sleep 1
 done
--- src/nodejs/extras/load_env.js
+++ src/nodejs/extras/load_env.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
 
 	app.get('/env', function(req, res){
-		const the_variable=process.env.THE_VAR || "default value";
+		const the_variable=process.env.THE_VAR || "default value"; // convict library to do this
 		res.send(`<div style="text-align: center;"> <h1 >The Variable is:<br> ${the_variable} </h1><br><h3>- NodeJS</h3> </div>` );
 	});
 
